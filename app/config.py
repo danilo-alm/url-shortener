7d3121b3faa4e15a2f63517c1f0d4afc21edb988
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-05-04 09:52:04 -0300

    fix: correct default value handling for API_PORT in config

--- app/config.py
+++ app/config.py
@@ -10,5 +10,5 @@ MONGO_PASSWORD = os.getenv('MONGO_PASSWORD', 'secret')
 MONGO_DATABASE = os.getenv('MONGO_DATABASE', 'url_shortener')
 MONGO_COLLECTION = os.getenv('MONGO_COLLECTION', 'urls')
 API_HOST = os.getenv('API_HOST', '127.0.0.1')
-API_PORT = int(os.getenv('API_PORT'), 8000)
+API_PORT = int(os.getenv('API_PORT', 8000))
 RELOAD_ON_CHANGE = os.getenv('RELOAD_ON_CHANGE', 'true').lower() == 'true'
