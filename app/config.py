1aacad17ef7014f17d0ce3759954c45ddb73ffee
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-05-03 22:15:59 -0300

    feat: set API_HOST through environment variable

--- app/config.py
+++ app/config.py
@@ -9,5 +9,6 @@ MONGO_USERNAME = os.getenv('MONGO_USERNAME')
 MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 MONGO_DATABASE = os.getenv('MONGO_DATABASE')
 MONGO_COLLECTION = os.getenv('MONGO_COLLECTION')
-FASTAPI_PORT = int(os.getenv('FASTAPI_PORT', 8000))
+API_HOST = os.getenv('API_HOST')
+API_PORT = int(os.getenv('API_PORT'))
 RELOAD_ON_CHANGE = os.getenv('RELOAD_ON_CHANGE', 'true').lower() == 'true'
--- app/run.py
+++ app/run.py
@@ -1,7 +1,7 @@
 import uvicorn
 
-from .config import FASTAPI_PORT, RELOAD_ON_CHANGE
+from .config import API_HOST, API_PORT, RELOAD_ON_CHANGE
 
 
 if __name__ == '__main__':
-    uvicorn.run('app.main:app', host='127.0.0.1', port=FASTAPI_PORT, reload=RELOAD_ON_CHANGE)
+    uvicorn.run('app.main:app', host=API_HOST, port=API_PORT, reload=RELOAD_ON_CHANGE)
