509fdc4ebb358799d72432b567be9d2d5abaf42c
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-05-03 23:15:21 -0300

    feat: set default values for env vars when running outside docker

--- app/config.py
+++ app/config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv('MONGO_URI')
-MONGO_USERNAME = os.getenv('MONGO_USERNAME')
-MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
-MONGO_DATABASE = os.getenv('MONGO_DATABASE')
-MONGO_COLLECTION = os.getenv('MONGO_COLLECTION')
-API_HOST = os.getenv('API_HOST')
-API_PORT = int(os.getenv('API_PORT'))
+MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
+MONGO_USERNAME = os.getenv('MONGO_USERNAME', 'admin')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD', 'secret')
+MONGO_DATABASE = os.getenv('MONGO_DATABASE', 'url_shortener')
+MONGO_COLLECTION = os.getenv('MONGO_COLLECTION', 'urls')
+API_HOST = os.getenv('API_HOST', '127.0.0.1')
+API_PORT = int(os.getenv('API_PORT'), 8000)
 RELOAD_ON_CHANGE = os.getenv('RELOAD_ON_CHANGE', 'true').lower() == 'true'
